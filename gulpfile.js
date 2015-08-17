3a2a38baf7ccd06cd2934d4471a19662057d370f
Pat O'Neill <pgoneill@gmail.com>
2015-08-16 20:06:24 -0400

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,71 @@
+'use strict';
+
+var babelify = require('babelify');
+var browserify = require('browserify');
+var del = require('del');
+var LessPluginAutoPrefix = require('less-plugin-autoprefix');
+var LessPluginCleanCSS = require('less-plugin-clean-css');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var gulp = require('gulp');
+var less = require('gulp-less');
+var gls = require('gulp-live-server');
+var sourcemaps = require('gulp-sourcemaps');
+var gutil = require('gulp-util');
+var uglify = require('gulp-uglify');
+
+gulp.task('clean', function (callback) {
+  del('dist', callback);
+});
+
+gulp.task('css', ['clean'], function () {
+  return gulp.src('./src/less/conway.less')
+    .pipe(less({
+      plugins: [
+        new LessPluginAutoPrefix({browsers: ['last 2 versions']}),
+        new LessPluginCleanCSS({advanced: true})
+      ]
+    }))
+    .pipe(gulp.dest('./dist/css/'));
+});
+
+gulp.task('html', ['clean'], function () {
+  return gulp.src('./src/index.html').pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('js', ['clean'], function () {
+  return browserify({
+    entries: './src/js/conway.js',
+    debug: true,
+    transform: [babelify]
+  }).bundle()
+    .pipe(source('conway.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(uglify())
+    .on('error', gutil.log)
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./dist/js/'));
+});
+
+gulp.task('watch', function () {
+  var server = gls.static('dist');
+
+  server.start();
+
+  gulp.watch([
+    'src/**/*.js',
+    'src/**/*.html',
+    'src/**/*.less'
+  ], [
+    'default'
+  ]);
+
+  gulp.watch([
+    'dist/**/*.*'
+  ], function () {
+    server.notify.apply(server, arguments);
+  });
+});
+
+gulp.task('default', ['css', 'html', 'js']);
