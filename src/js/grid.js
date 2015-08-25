01eee59620753c63a30c51fadb3555b87c1ec7bc
Pat O'Neill <pgoneill@gmail.com>
2015-08-24 20:00:04 -0400

    Adjust grid data as needed.

--- src/js/grid.js
+++ src/js/grid.js
@@ -6,36 +6,29 @@ const CELL_SIZE = 10;
 const CELL_OFFSET = CELL_GAP + CELL_SIZE;
 
 const COLOR_GRID = 'rgba(0, 0, 0, 1)';
-const COLOR_CELL_DEAD = 'rgba(255, 255, 255, 0.333)';
+const COLOR_CELL_DEAD = 'rgba(255, 255, 255, 0.5)';
 const COLOR_CELL_ALIVE = 'rgba(255, 255, 255, 1)';
+
+function resize() {
+  const size = this.getSize();
+  dom.setAttrs(this.el, size);
+  this.adjustDataSize(size).draw();
+}
 
 export default class Grid {
 
-  constructor() {
-    this._data = [
-      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    ];
+  data() {
+    if (!Array.isArray(this._data) || !Array.isArray(this._data[0])) {
+      this._data = [[0]];
+    }
+    return this._data;
   }
 
   setup() {
     this.el = document.createElement('canvas');
     this.context = this.el.getContext('2d');
 
-    this.resize = _.throttle(function resize() {
-      dom.setAttrs(this.el, this.getSize());
-      this.draw();
-    }, 250).bind(this);
-
-    this.resize();
+    this.resize = _.throttle(resize, 500).bind(this);
     window.addEventListener('resize', this.resize);
 
     this.attach();
@@ -61,14 +54,14 @@ export default class Grid {
   }
 
   draw() {
-    let size = this.getSize();
+    const size = this.getSize();
 
     this.context.fillStyle = COLOR_GRID;
     this.context.fillRect(0, 0, size.width, size.height);
 
     let y = CELL_GAP;
 
-    this._data.forEach(function (row) {
+    this.data().forEach(function (row) {
       let x = CELL_GAP;
 
       row.forEach(function (cell) {
@@ -77,10 +70,10 @@ export default class Grid {
         this.context[type + 'Style'] = cell ? COLOR_CELL_ALIVE : COLOR_CELL_DEAD;
         this.context[type + 'Rect'](x, y, CELL_SIZE, CELL_SIZE);
 
-        x += CELL_SIZE + CELL_GAP;
+        x += CELL_OFFSET;
       }, this);
 
-      y += CELL_SIZE + CELL_GAP;
+      y += CELL_OFFSET;
     }, this);
 
     return this;
@@ -91,5 +84,40 @@ export default class Grid {
       height: document.body.clientHeight,
       width: document.body.clientWidth
     };
+  }
+
+  adjustDataSize(size) {
+    let data = this.data();
+
+    let rowCountNew = Math.floor((size.height - CELL_GAP) / CELL_OFFSET);
+    let rowCountOld = data.length;
+    let rowDiff = rowCountNew - rowCountOld;
+    let colCountNew = Math.floor((size.width - CELL_GAP) / CELL_OFFSET);
+    let colCountOld = data[0].length;
+    let colDiff = colCountNew - colCountOld;
+
+    // Adding new row(s) of dead cells.
+    if (rowDiff > 0) {
+      for (let i = rowCountOld; i < rowCountNew; i++) {
+        data[i] = _.fill(Array(colCountNew), 0);
+      }
+
+    // Removing old row(s).
+    } else if (rowDiff < 0) {
+      data.length += rowDiff;
+    }
+
+    // Adding or removing dead cells from existing row(s).
+    if (colDiff !== 0) {
+      for (let i = 0; i < Math.min(rowCountOld, data.length); i++) {
+        if (colDiff > 0) {
+          data[i] = data[i].concat(_.fill(Array(colDiff), 0));
+        } else {
+          data[i].length += colDiff;
+        }
+      }
+    }
+
+    return this;
   }
 }
