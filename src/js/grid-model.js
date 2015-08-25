2511bee71f1b398569154afe77d0215bb33cc428
Pat O'Neill <pgoneill@gmail.com>
2015-08-25 14:04:22 -0400

    Flesh out model tests

--- src/js/grid-model.js
+++ src/js/grid-model.js
@@ -3,20 +3,72 @@ import _ from 'lodash';
 // Private data store for raw grid data.
 const store = new WeakMap();
 
+/**
+ * Represents a two-dimensional grid of binary cells (ones and zeros).
+ *
+ * By default, it is populated with a 1x1 grid containing a single zero.
+ *
+ * @class GridModel
+ */
 export default class GridModel {
 
+  /**
+   * GridModel constructor.
+   *
+   * @constructor
+   * @param       {Array} data Provide initial data for the model.
+   */
   constructor (data) {
-    store.set(this, Array.isArray(data) ? _.cloneDeep(data) : [[0]]);
+    this.populate(Array.isArray(data) ? _.cloneDeep(data) : [[0]]);
   }
 
+  /**
+   * Get a deep clone of the model's data.
+   *
+   * @method data
+   * @return {Array}
+   */
   data () {
+    let data = store.get(this);
+    if (!data) {
+      this.populate([[0]]);
+    }
     return _.cloneDeep(store.get(this));
   }
 
+  /**
+   * Destroy this model's data.
+   *
+   * @method destroy
+   */
   destroy () {
     store.delete(this);
   }
 
+  /**
+   * Populate the model with entirely new data.
+   *
+   * @method populate
+   * @param  {Array} data
+   */
+  populate (data) {
+    if (!Array.isArray(data)) {
+      throw new Error('data must be an array');
+    }
+    if (!Array.isArray(data[0]) || !_.isNumber(data[0][0])) {
+      throw new Error('data[0] must be an array with at least one number');
+    }
+    store.set(this, data);
+  }
+
+  /**
+   * Resize the model data. Newly created rows/cells will be populated with
+   * zeros.
+   *
+   * @method resize
+   * @param  {Number} rowCountNew
+   * @param  {Number} colCountNew
+   */
   resize (rowCountNew, colCountNew) {
     let data = store.get(this);
     let rowCountOld = data.length;
@@ -37,6 +89,9 @@ export default class GridModel {
 
     // Adding or removing dead cells from existing row(s).
     if (colDiff !== 0) {
+
+      // The `Math.min` call here ensures that we don't iterate too far if
+      // the number of rows was reduced.
       for (let i = 0; i < Math.min(rowCountOld, data.length); i++) {
         if (colDiff > 0) {
           data[i] = data[i].concat(_.fill(Array(colDiff), 0));
--- tests/grid-model.test.js
+++ tests/grid-model.test.js
@@ -1,15 +1,48 @@
+import _ from 'lodash';
 import assert from 'assert';
 import GridModel from '../src/js/grid-model';
 
 const data = [
-  [0, 1, 0],
-  [1, 0, 0],
-  [0, 0, 0],
+  [0, 1, 0, 1],
+  [1, 0, 0, 1],
+  [0, 0, 0, 1],
+  [0, 1, 1, 0]
 ];
 
-const model = new GridModel(data);
+let model;
 
 describe('GridModel', () => {
+
+  beforeEach(() => {
+    model = new GridModel(data);
+  });
+
+  describe('#populate()', () => {
+
+    it('should throw an error if given a bad value', () => {
+      assert.throws(() => {
+        model.populate(null);
+      });
+
+      assert.throws(() => {
+        model.populate({});
+      });
+
+      assert.throws(() => {
+        model.populate([]);
+      });
+
+      assert.throws(() => {
+        model.populate([[]]);
+      });
+    });
+
+    it('should accept an array with at least one array element with at least one number element', () => {
+      assert.doesNotThrow(() => {
+        model.populate([[1]]);
+      });
+    });
+  });
 
   describe('#data()', () => {
 
@@ -20,6 +53,29 @@ describe('GridModel', () => {
     it('should never expose internal data array as mutable', () => {
       assert.notStrictEqual(model.data(), data);
       assert.notStrictEqual(model.data(), model.data());
+    });
+  });
+
+  describe('#resize()', () => {
+
+    it('should add new rows and cells', () => {
+      model.resize(5, 5);
+      let data = model.data();
+      assert.equal(data.length, 5);
+      assert.equal(data[0].join('-'), '0-1-0-1-0');
+      assert.equal(data[1].join('-'), '1-0-0-1-0');
+      assert.equal(data[2].join('-'), '0-0-0-1-0');
+      assert.equal(data[3].join('-'), '0-1-1-0-0');
+      assert.equal(data[4].join('-'), '0-0-0-0-0');
+    });
+
+    it('should remove rows and cells', () => {
+      model.resize(3, 3);
+      let data = model.data();
+      assert.equal(data.length, 3);
+      assert.equal(data[0].join('-'), '0-1-0');
+      assert.equal(data[1].join('-'), '1-0-0');
+      assert.equal(data[2].join('-'), '0-0-0');
     });
   });
 });
